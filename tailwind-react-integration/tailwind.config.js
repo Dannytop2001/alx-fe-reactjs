ffce1806d1be2a0d16e440c737e40498c67917c3
Dannytop2001 <oketolad@gmail.com>
2024-12-05 13:36:35 +0100

    tailwindcss

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Add this to scan your source files for Tailwind classes
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Updated from "purge" to "content"
+  darkMode: false, // You can use 'media' or 'class' for dark mode
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
